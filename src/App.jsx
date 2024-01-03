a3a619b3ce3503868c97b4cc70110f8bb26bbd5d
Pascal Albisser <pascal@pascals-mbp.home>
2024-01-03 12:31:00 +0100

    start template

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,23 @@ function App() {
 
     return (
         <>
-            <header>
-                <h1 className={'font-bold'}>Barking emotions</h1>
-            </header>
-            <main>
-                <p>Record your dog's barking and find out how he feels like</p>
-                <div>
-                    Recorder
-                </div>
-            </main>
+            <div className={'flex flex-col h-screen'}>
+                <header className={'bg-slate-50 p-4'}>
+                    <h1 className={'font-bold max-w-prose mx-auto text-center'}>Barking emotions</h1>
+                </header>
+                <main className={'grow p-4'}>
+                    <p className={'max-w-prose mx-auto px-2'}>Record your dog's barking and find out how he feels like</p>
+                    <div className={'mx-auto text-center p-4'}>
+                        <button className={'bg-red-100 p-2 rounded shadow-sm'}>
+                            <span className={'inline-block rounded-lg bg-red-500 w-3 h-3 mx-1'}></span>
+                            Record
+                        </button>
+                    </div>
+                </main>
+                <footer className={'bg-slate-700 py-2 px-2'}>
+                    <p className={'max-w-prose mx-auto text-slate-50 text-sm'}>Lorem ipsum</p>
+                </footer>
+            </div>
         </>
     )
 }
